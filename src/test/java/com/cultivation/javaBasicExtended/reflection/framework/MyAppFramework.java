f05616fd2e0b75df1bd23201bef8f4bf3d3bcb49
DH-TB <929659475@qq.com>
2018-08-23 18:25:11 +0800

    should_invoke_request
    Signed-off-by: DH-TB <929659475@qq.com>

--- src/test/java/com/cultivation/javaBasicExtended/reflection/framework/MyAppFramework.java
+++ src/test/java/com/cultivation/javaBasicExtended/reflection/framework/MyAppFramework.java
@@ -1,7 +1,7 @@
 package com.cultivation.javaBasicExtended.reflection.framework;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -22,10 +22,18 @@ public class MyAppFramework {
         // --end-->
     }
 
-    public Response getResponse(String requestController, String requestMethod) {
+    public Response getResponse(String requestController, String requestMethod) throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
         // TODO: Please implement the method
         // <--start
-        throw new NotImplementedException();
+        Class aClass = controllerList.stream().filter(c -> c.getName().equals(requestController)).findAny().orElse(null);
+        if(Objects.nonNull(aClass)){
+            Method declaredMethod = aClass.getDeclaredMethod(requestMethod);
+            Object instance = aClass.newInstance();
+
+            return (Response) declaredMethod.invoke(instance);
+        }
+
+        return null;
         // --end-->
     }
 
