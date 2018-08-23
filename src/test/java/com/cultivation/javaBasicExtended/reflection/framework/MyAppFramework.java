682067817f603b2a0d777420798812aa52f8e89d
DH-TB <929659475@qq.com>
2018-08-23 18:13:10 +0800

    finish test should_throw_if_register_duplicated_controller_clazz
    Signed-off-by: DH-TB <929659475@qq.com>

--- src/test/java/com/cultivation/javaBasicExtended/reflection/framework/MyAppFramework.java
+++ src/test/java/com/cultivation/javaBasicExtended/reflection/framework/MyAppFramework.java
@@ -2,12 +2,23 @@ package com.cultivation.javaBasicExtended.reflection.framework;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @SuppressWarnings("WeakerAccess")
 public class MyAppFramework {
+    private List<Class> controllerList = new ArrayList<>();
     public void registerController(Class controllerClazz) {
         // TODO: Please implement the method
         // <--start
-        throw new NotImplementedException();
+
+        Class aClass = controllerList.stream().filter(c -> c == controllerClazz).findAny().orElse(null);
+        if(Objects.nonNull(aClass)){
+            throw new IllegalArgumentException();
+        }
+        controllerList.add(controllerClazz);
+
         // --end-->
     }
 
